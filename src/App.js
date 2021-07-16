3711f51b2248a9554f68662942f0950e764148d6
Radensuaso <andrels9283@gmail.com>
2021-07-16 09:01:16 +0100

    Navbar in progress

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
+import { BrowserRouter as Router } from "react-router-dom"
+import NavSpotify from "./components/NavSpotify"
 
 const App = () => {
-  return <div className="App"></div>
+  return (
+    <Router>
+      <div className="App">
+        <NavSpotify />
+      </div>
+    </Router>
+  )
 }
 
 export default App
---
+++ src/components/NavSpotify.jsx
@@ -0,0 +1,7 @@
+import { withRouter } from "react-router-dom"
+
+const NavSpotify = () => {
+  return <nav id="nav-spotify" className="d-flex flex-column"></nav>
+}
+
+export default withRouter(NavSpotify)
