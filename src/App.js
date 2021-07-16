cc88b99a36e105341163b87293dffd6acf12dc1a
Radensuaso <andrels9283@gmail.com>
2021-07-16 11:51:52 +0100

    Footer complete

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
 import { BrowserRouter as Router } from "react-router-dom"
 import NavSpotify from "./components/NavSpotify"
+import Player from "./components/Player"
 
 const App = () => {
   return (
     <Router>
       <div className="App">
         <NavSpotify />
+        <Player />
       </div>
     </Router>
   )
