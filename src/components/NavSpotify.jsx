ad95fa564767b65348186291a3a3984751938496
Radensuaso <andrels9283@gmail.com>
2021-07-16 11:18:01 +0100

    Navbar complete

--- src/components/NavSpotify.jsx
+++ src/components/NavSpotify.jsx
@@ -1,7 +1,53 @@
 import { withRouter } from "react-router-dom"
+import spotifyLogo from "../src-assets/spotify-logo.png"
+import { HiHome } from "react-icons/hi"
+import { BsSearch } from "react-icons/bs"
+import { ImBooks } from "react-icons/im"
+import Button from "react-bootstrap/Button"
 
 const NavSpotify = () => {
-  return <nav id="nav-spotify" className="d-flex flex-column"></nav>
+  return (
+    <nav
+      id="nav-spotify"
+      className="d-flex flex-column justify-content-between"
+    >
+      <div>
+        <div className="mx-3 my-4">
+          <img src={spotifyLogo} alt="spotify logo" />
+        </div>
+        <div className="nav-spotify-link mx-3 mb-3 d-flex align-items-center">
+          <span className="menu-icon mr-3 d-flex align-items-center">
+            <HiHome />
+          </span>
+          <strong className="menu-description">Home</strong>
+        </div>
+        <div className="nav-spotify-link mx-3 mb-3 d-flex align-items-center">
+          <span className="menu-icon mr-3 d-flex align-items-center">
+            <BsSearch />
+          </span>
+          <strong className="menu-description">Search</strong>
+        </div>
+        <div className="nav-spotify-link mx-3 mb-3 d-flex align-items-center">
+          <span className="menu-icon mr-3 d-flex align-items-center">
+            <ImBooks />
+          </span>
+          <strong className="menu-description">Your Library</strong>
+        </div>
+      </div>
+      <div className="nav-spotify-bottom text-center">
+        <Button className="nav-button mb-3" variant="outline-light">
+          SIGN UP
+        </Button>
+        <Button className="nav-button mb-3" variant="outline-light">
+          Login
+        </Button>
+        <div>
+          <p>Cookie | Privacy</p>
+          <p>Policy</p>
+        </div>
+      </div>
+    </nav>
+  )
 }
 
 export default withRouter(NavSpotify)
