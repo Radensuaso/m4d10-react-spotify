2c2bb55cbd5a5fce0fddccd4efe52693a6926c4e
Radensuaso <andrels9283@gmail.com>
2021-07-26 15:25:24 +0100

    Homework complete

--- src/components/AlbumPage.jsx
+++ src/components/AlbumPage.jsx
@@ -2,7 +2,10 @@ import Container from "react-bootstrap/Container"
 import { useState, useEffect } from "react"
 import fetchGet from "../functions/fetchGet"
 import { usePalette } from "react-palette"
-import AlbumJumbotron from "./AlbulmJumbotron"
+import AlbumJumbotron from "./AlbumJumbotron"
+import Table from "react-bootstrap/Table"
+import { AiOutlineClockCircle } from "react-icons/ai"
+import { Link } from "react-router-dom"
 
 const AlbumPage = (props) => {
   const [album, setAlbum] = useState({
@@ -35,7 +38,49 @@ const AlbumPage = (props) => {
               rgba(18, 18, 18, 1) 20%
             )`,
         }}
-      ></Container>
+      >
+        <Table responsive>
+          <thead className="table-head">
+            <tr>
+              <th className="d-flex">
+                <div className="mr-3">#</div>
+                <div>TITLE</div>
+              </th>
+              <th>
+                <AiOutlineClockCircle />
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {album.data &&
+              album.data.tracks.data.map((track, i) => (
+                <tr className="song-table-row" key={i}>
+                  <td className="d-flex">
+                    <div className="d-flex justify-content-center align-items-center mr-3">
+                      {i + 1}
+                    </div>
+                    <div>
+                      <span className="song-title">{track.title}</span> <br />
+                      <span>
+                        <Link
+                          style={{ color: "#b3b3b3" }}
+                          to={`/artist/${track.artist.id}`}
+                        >
+                          {track.artist.name}
+                        </Link>
+                      </span>
+                    </div>
+                  </td>
+                  <td>{`${Math.floor(track.duration / 60)}:${(
+                    track.duration -
+                    Math.floor(track.duration / 60) * 60 +
+                    "0"
+                  ).substring(0, 2)}`}</td>
+                </tr>
+              ))}
+          </tbody>
+        </Table>
+      </Container>
     </>
   )
 }
